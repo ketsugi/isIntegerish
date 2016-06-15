363cc0809904832060f02ed3f6ed30341b170e13
Joel Pan <spamtastic@ketsugi.com>
2016-06-15 22:57:56 +0800

    Add test module

---
+++ test/index.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+
+const isIntegerish = require('../isIntegerish');
+
+describe('isIntegerish:', function() {
+  it('should return true if an integer value is passed', function() {
+    expect(isIntegerish(0)).to.be.true;
+    expect(isIntegerish(10)).to.be.true;
+    expect(isIntegerish(42)).to.be.true;
+    expect(isIntegerish(-999)).to.be.true;
+  });
+
+  it('should return true if a string of an integer value is passed', function() {
+    expect(isIntegerish('0')).to.be.true;
+    expect(isIntegerish('10')).to.be.true;
+    expect(isIntegerish('42')).to.be.true;
+    expect(isIntegerish('-999')).to.be.true;
+  });
+
+  it('should return false if a non-integer number is passed', function() {
+    expect(isIntegerish(0.5)).to.be.false;
+    expect(isIntegerish(10.2)).to.be.false;
+    expect(isIntegerish(4.2)).to.be.false;
+    expect(isIntegerish(9.99)).to.be.false;
+    expect(isIntegerish(-100.2)).to.be.false;
+  })
+
+  it('should return false if a string of a non-integer number is passed', function() {
+    expect(isIntegerish('0.5')).to.be.false;
+    expect(isIntegerish('10.2')).to.be.false;
+    expect(isIntegerish('4.2')).to.be.false;
+    expect(isIntegerish('9.99')).to.be.false;
+    expect(isIntegerish('-100.2')).to.be.false;
+  })
+
+  it('should return false if a string of a non-number is passed', function() {
+    expect(isIntegerish('abc')).to.be.false;
+    expect(isIntegerish('def')).to.be.false;
+    expect(isIntegerish('the quick brown fox')).to.be.false;
+    expect(isIntegerish('jumps over the lazy dog!')).to.be.false;
+    expect(isIntegerish('another test just to make 5')).to.be.false;
+  })
+});
