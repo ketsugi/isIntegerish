d80b85c1143545db1f4dc2d34e155b989e2ffa91
Joel Pan <spamtastic@ketsugi.com>
2016-06-15 23:13:08 +0800

    Add test for integer-like floats

--- test/index.js
+++ test/index.js
@@ -10,6 +10,13 @@ describe('isIntegerish:', function() {
     expect(isIntegerish(-999)).to.be.true;
   });
 
+  it('should return true if a float value that is also an integer is passed', function() {
+    expect(isIntegerish(0.0)).to.be.true;
+    expect(isIntegerish(10.0)).to.be.true;
+    expect(isIntegerish(42.0)).to.be.true;
+    expect(isIntegerish(-999.0)).to.be.true;
+  });
+
   it('should return true if a string of an integer value is passed', function() {
     expect(isIntegerish('0')).to.be.true;
     expect(isIntegerish('10')).to.be.true;
@@ -19,7 +26,6 @@ describe('isIntegerish:', function() {
 
   it('should return false if a non-integer number is passed', function() {
     expect(isIntegerish(0.5)).to.be.false;
-    expect(isIntegerish(10.2)).to.be.false;
     expect(isIntegerish(4.2)).to.be.false;
     expect(isIntegerish(9.99)).to.be.false;
     expect(isIntegerish(-100.2)).to.be.false;
@@ -27,7 +33,6 @@ describe('isIntegerish:', function() {
 
   it('should return false if a string of a non-integer number is passed', function() {
     expect(isIntegerish('0.5')).to.be.false;
-    expect(isIntegerish('10.2')).to.be.false;
     expect(isIntegerish('4.2')).to.be.false;
     expect(isIntegerish('9.99')).to.be.false;
     expect(isIntegerish('-100.2')).to.be.false;
@@ -38,6 +43,5 @@ describe('isIntegerish:', function() {
     expect(isIntegerish('def')).to.be.false;
     expect(isIntegerish('the quick brown fox')).to.be.false;
     expect(isIntegerish('jumps over the lazy dog!')).to.be.false;
-    expect(isIntegerish('another test just to make 5')).to.be.false;
   })
 });
